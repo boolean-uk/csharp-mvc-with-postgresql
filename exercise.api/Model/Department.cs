197a751a8ec021cfd4a686aae9788bba67f20e4e
Nikita Pieters <nikita_pieters@hotmail.com>
2023-06-06 15:31:48 +0200

    added models for salary and department

---
+++ exercise.api/Model/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace exercise.api.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
