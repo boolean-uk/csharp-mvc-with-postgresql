53c5160f9858b225c6ae7ce0705ada30119b7886
joeri luteyn <joeriluteyn@gmail.com>
2023-06-06 17:14:33 +0200

    Core requirements done

---
+++ exercise.api/EndPoint/EmployeeApi.cs
@@ -0,0 +1,89 @@
+﻿using exercise.api.Models;
+using exercise.api.Repository;
+
+namespace exercise.api.EndPoint
+{
+    public static class EmployeeApi
+    {
+        public static void ConfigureEmployeeApi(this WebApplication app) 
+        {
+            app.MapGet("/employees", GetEmployees);
+            app.MapGet("/employees/{id}", GetEmployee);
+            app.MapPost("/employees", InsertEmployee);
+            app.MapDelete("/employees/{id}", DeleteEmployee);
+            app.MapPut("/employees/{id}", UpdateEmployee);
+
+        }
+
+        private static async Task<IResult> UpdateEmployee(IEmployeeRepository repo, int id, Employee employee)
+        {
+            try
+            {
+                
+                return repo.UpdateEmployee(employee, id) != null ? Results.Ok(employee) : Results.NotFound();
+            }
+
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+
+        private static async Task<IResult> DeleteEmployee(IEmployeeRepository repo, int id)
+        {
+            try
+            {
+                var result = repo.DeleteEmployee(id);
+                return result != null ? Results.Ok(result) : Results.NotFound();
+            }
+
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> InsertEmployee(IEmployeeRepository repo, Employee employee)
+        {
+            try
+            {
+                return Results.Ok(repo.AddEmployee(employee));
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> GetEmployee(IEmployeeRepository repo, int id)
+        {
+            try
+            {
+                var result = repo.GetEmployeeById(id);
+                return result != null ? Results.Ok(result) : Results.NotFound();
+            }
+
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+    
+
+        private static async Task<IResult> GetEmployees(IEmployeeRepository repo)
+        {
+            try
+            {
+                return Results.Ok(repo.GetEmployees());
+            }
+
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ exercise.api/Repository/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using exercise.api.Models;
+
+namespace exercise.api.Repository
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetEmployees();
+        Employee AddEmployee(Employee employee);
+        Employee GetEmployeeById(int id);
+        IEnumerable<Employee> DeleteEmployee(int id);
+        Employee UpdateEmployee(Employee employee, int id);
+
+    }
+}
