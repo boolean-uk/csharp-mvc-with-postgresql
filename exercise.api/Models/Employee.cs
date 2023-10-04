1ff055a06de85f47679c8e79602796c6d9a55be0
Ävadakedavr0 <dylandd4@hotmail.be>
2023-10-05 01:12:37 +0200

    core exercise done, changed from ElephantSQL to Heroku for PostgreSQL version newer then 10.0 - id needs to be exluded and required fields needs to be added

--- exercise.api/Models/Employee.cs
+++ exercise.api/Models/Employee.cs
@@ -1,7 +1,10 @@
-﻿namespace exercise.api.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace exercise.api.Models
 {
     public class Employee
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string JobName { get; set; }
